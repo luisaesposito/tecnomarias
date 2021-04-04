fbe68d5669b64401ca2844322750ecdc29298598
Lucas Rogério <lucasrogerio@id.uff.br>
2021-04-04 18:35:39 -0300

    ajuste no JacksonFeature

--- src/main/java/br/uff/tecnomarias/JacksonFeature.java
+++ src/main/java/br/uff/tecnomarias/JacksonFeature.java
@@ -1,5 +1,6 @@
 package br.uff.tecnomarias;
 
+import com.fasterxml.jackson.jaxrs.base.JsonMappingExceptionMapper;
 import com.fasterxml.jackson.jaxrs.base.JsonParseExceptionMapper;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
@@ -7,13 +8,14 @@ import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
 
+@Provider
 public class JacksonFeature implements Feature {
-
     @Override
     public boolean configure(FeatureContext context) {
-        // add the default Jackson exception mappers and allow jaxb annotations
         context.register(JsonParseExceptionMapper.class);
+        context.register(JsonMappingExceptionMapper.class);
         context.register(JacksonJaxbJsonProvider.class, MessageBodyReader.class, MessageBodyWriter.class);
         return true;
     }
--- src/main/java/br/uff/tecnomarias/RESTApplication.java
+++ src/main/java/br/uff/tecnomarias/RESTApplication.java
@@ -1,28 +1,17 @@
 package br.uff.tecnomarias;
 
-import br.uff.tecnomarias.rest.resource.HealthResource;
-import br.uff.tecnomarias.rest.resource.VagaResource;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @ApplicationPath("api")
 public class RESTApplication extends Application {
 
     @Override
-    public Set<Class<?>> getClasses() {
-        return new HashSet<>(Arrays.asList(VagaResource.class));
-    }
-
-    @Override
     public Map<String, Object> getProperties() {
         final Map<String, Object> properties = new HashMap<String, Object>();
         properties.put("jersey.config.server.disableMoxyJson", true);
-
         return properties;
     }
 
