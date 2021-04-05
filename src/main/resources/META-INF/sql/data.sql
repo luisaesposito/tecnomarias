10e3966b7b699ae27443908d77adbf06e8c9d7cf
Renan Barbosa <renan.barbosa@plataformaamais.com.br>
2021-04-05 01:55:02 -0300

    Adicionando tipo de pessoa ao insert

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -1,15 +1,15 @@
 INSERT INTO public.pessoa(id, email, nome) VALUES (1, 'teste@id.uff.br', 'teste');
 INSERT INTO public.pessoajuridica(id, areaatuacao, cnpj, descricao, porteempresa) VALUES (1, 'cosmeticos', '36485199000132', 'cd', 'MICROEMPRESA');
 
-INSERT INTO PUBLIC.pessoa (id,nome,email) values(11, 'Graziela de Jesus', 'grazielajj@gmail.com');
+INSERT INTO PUBLIC.pessoa (id,nome,tipo_pessoa,email) values(11, 'Graziela de Jesus','PF', 'grazielajj@gmail.com');
 INSERT INTO PUBLIC.links (id,linkedin, github, portifolio, facebook) values (11,'linkedIn.com/grazielajj','github.com/grazielajj','seuportifolio.com/grazielajj','facebook.com/grazielajj');
 INSERT INTO PUBLIC.pessoafisica (id,links_id) values(11,11);
 
-INSERT INTO PUBLIC.pessoa (id,nome,email) values(12, 'Maria Antonieta Da Paz', 'mariaantonieta@gmail.com');
+INSERT INTO PUBLIC.pessoa (id,nome,tipo_pessoa, email) values(12, 'Maria Antonieta Da Paz','PF', 'mariaantonieta@gmail.com');
 INSERT INTO PUBLIC.links (id,linkedin, github, portifolio, facebook) values (11,'linkedIn.com/mariaantonieta','github.com/mariaantonieta','seuportifolio.com/mariaantonieta','facebook.com/mariaantonieta');
 INSERT INTO PUBLIC.pessoafisica (id,links_id) values(12,12);
 
-INSERT INTO PUBLIC.pessoa (id,nome,email) values(13, 'Estrela da Silva', 'estrela@gmail.com');
+INSERT INTO PUBLIC.pessoa (id,nome,tipo_pessoa,email) values(13, 'Estrela da Silva','PF', 'estrela@gmail.com');
 INSERT INTO PUBLIC.links (id,linkedin, github, portifolio, facebook) values (13,'linkedIn.com/estrela','github.com/estrela','seuportifolio.com/estrela','facebook.com/estrela');
 INSERT INTO PUBLIC.pessoafisica (id,links_id) values(13,13);
 
