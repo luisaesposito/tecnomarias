40cab9e1ab0852f65438a19064928830fc3b6cb3
Renan Barbosa <renan.barbosa@plataformaamais.com.br>
2021-04-05 01:43:37 -0300

    SQL com insert de PF

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -1,2 +1,14 @@
 INSERT INTO public.pessoa(id, email, nome) VALUES (1, 'teste@id.uff.br', 'teste');
 INSERT INTO public.pessoajuridica(id, areaatuacao, cnpj, descricao, porteempresa) VALUES (1, 'cosmeticos', '36485199000132', 'cd', 'MICROEMPRESA');
+
+INSERT INTO PUBLIC.pessoa (id,nome,email) values(11, 'Joao Augusto Ferreira', 'joaoaugusto@gmail.com')
+    INSERT INTO PUBLIC.links (id,linkedin, github, portifolio, facebook) values (11,'linkedIn.com/joaoausgusto','github.com/joaoausgusto','seuportifolio.com/joaoausgusto','facebook.com/joaoausgusto')
+INSERT INTO PUBLIC.pessoafisica (id,links_id) values(11,11)
+
+INSERT INTO PUBLIC.pessoa (id,nome,email) values(12, 'Maria Antonieta Da Paz', 'mariaantonieta@gmail.com')
+INSERT INTO PUBLIC.links (id,linkedin, github, portifolio, facebook) values (11,'linkedIn.com/mariaantonieta','github.com/mariaantonieta','seuportifolio.com/mariaantonieta','facebook.com/mariaantonieta')
+INSERT INTO PUBLIC.pessoafisica (id,links_id) values(12,12)
+
+INSERT INTO PUBLIC.pessoa (id,nome,email) values(13, 'Enzo Estrela da Silva', 'enzoestrela@gmail.com')
+INSERT INTO PUBLIC.links (id,linkedin, github, portifolio, facebook) values (13,'linkedIn.com/enzoestrela','github.com/enzoestrela','seuportifolio.com/enzoestrela','facebook.com/enzoestrela')
+INSERT INTO PUBLIC.pessoafisica (id,links_id) values(13,13)
