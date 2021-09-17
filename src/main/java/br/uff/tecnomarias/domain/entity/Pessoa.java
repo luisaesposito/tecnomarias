dc65c19e35c162486c3b8add690f86dd7d8d535b
Luisa Esposito <luisa_esposito@id.uff.br>
2021-09-17 19:39:20 -0300

    corrige validação de string para not blank

--- src/main/java/br/uff/tecnomarias/domain/entity/Pessoa.java
+++ src/main/java/br/uff/tecnomarias/domain/entity/Pessoa.java
@@ -5,6 +5,7 @@ import br.uff.tecnomarias.domain.utils.EntityUtils;
 
 import javax.persistence.*;
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
@@ -19,10 +20,10 @@ public class Pessoa {
     @Column(name = "id", updatable = false, nullable = false)
     protected Long id;
 
-    @NotNull(message = "nome é obrigatório")
+    @NotBlank(message = "nome é obrigatório")
     protected String nome;
 
-    @NotNull(message = "email é obrigatório")
+    @NotBlank(message = "email é obrigatório")
     protected String email;
 
     @NotNull(message = "tipo pessoa é obrigatório")
